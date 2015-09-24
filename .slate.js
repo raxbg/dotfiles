53f63510c092a9c9d3f0e469b478f3afe095c1b4
Ivailo Hristov <ivailo.hristov@isenselabs.com>
2015-09-24 18:30:18 +0300

    Window movement improvements + attempt at auto-tiling functionality

--- .slate.js
+++ .slate.js
@@ -94,27 +94,25 @@ var leftFullHeight = slate.operation("move", {
 });
 
 var rightFullHeight = slate.operation("move", {
-	"x" : "screenSizeX/2",
+	"x" : "screenOriginX+(screenSizeX/2)",
 	"y" : "screenOriginY",
 	"width" : "screenSizeX/2",
 	"height" : "screenSizeY"
 });
 
-var throwLeftFullScreen = slate.operation("throw", {
-	"x" : "screenOriginX",
-	"y" : "screenOriginY",
-	"width" : "screenSizeX",
-	"height" : "screenSizeY",
-	"screen" : 0
-});
-
-var throwRightFullScreen = slate.operation("throw", {
-	"x" : "screenOriginX",
-	"y" : "screenOriginY",
-	"width" : "screenSizeX",
-	"height" : "screenSizeY",
-	"screen" : 1
-});
+var throwFullScreen = function(direction, win) {
+  var screensCount = slate.screenCount();
+  var currentScreen = win.screen().id();
+  var newScreen = (currentScreen+direction) % screensCount;
+  newScreen = (newScreen < 0) ? (screensCount-1) : newScreen;
+  win.doOperation("throw", {
+    "x" : "screenOriginX",
+    "y" : "screenOriginY",
+    "width" : "screenSizeX",
+    "height" : "screenSizeY",
+    "screen" : newScreen
+  });
+};
 
 var fullScreen = slate.operation("move", {
 	"x" : "screenOriginX",
@@ -140,6 +138,77 @@ slate.bind("f:alt", hint);
 var relaunch = slate.operation("relaunch");
 slate.bind("r:alt", relaunch);
 
+var tileAll = function() {
+  var scr = slate.screen();
+
+  var rect = scr.visibleRect();
+  var topLeftX = rect.x;
+  var topLeftY = rect.y;
+  var scrWidth = rect.width;
+  var scrHeight = rect.height;
+
+  var totalWindows = 0;
+  slate.eachApp(function(app) {
+    app.eachWindow(function(win) {
+      if (win.screen().id() == scr.id() && !win.isMinimizedOrHidden() && win.title()) {
+        totalWindows++;
+      }
+    });
+  });
+  slate.log('total:: ' + totalWindows);
+
+  if (totalWindows == 1) {
+  } else if (totalWindows > 1) {
+    var winCountLeft = parseInt(totalWindows / 2);
+    var winCountRight = (totalWindows % 2 === 0) ? winCountLeft : winCountLeft+1;
+    var posWindowsCounts = [0,0];
+    var pos = 'left';
+
+    slate.eachApp(function(app) {
+      app.eachWindow(function(win) {
+        if (win.screen().id() == scr.id()) {
+          var winCounts, sideWinCounts;
+          switch (pos) {
+            case 'left':
+              winCounts = posWindowsCounts[0];
+              posWindowsCounts[0]++;
+              sideWinCounts = winCountLeft;
+              break;
+            case 'right':
+              winCounts = posWindowsCounts[1];
+              posWindowsCounts[1]++;
+              sideWinCounts = winCountRight;
+              break;
+          }
+
+          var x = (pos == 'left') ? topLeftX : topLeftX+scrWidth/2;
+          var y = topLeftY + (scrHeight / sideWinCounts * winCounts);
+          var width = scrWidth/2;
+          var height = scrHeight / sideWinCounts;
+
+          win.doOperation("move", {
+            "x" : x,
+            "y" : y,
+            "width" : width,
+            "height" : height,
+            "screen" : scr.id()
+          });
+
+          pos = (pos == 'left') ? 'right' : 'left';
+          if (pos == 'left' && posWindowsCounts[0] == winCountLeft) pos = 'right';
+        }
+      });
+    });
+  }
+};
+
+slate.on('windowOpened', function(e, win) {
+  if (win.title().match(/^chrome-devtools/)) {
+    win.doOperation(throwFullScreen(1, win));
+  }
+  tileAll();
+});
+
 var grid = slate.operation("grid", {
 	"grids" : {
 		"1920x1080" : {
@@ -149,6 +218,7 @@ var grid = slate.operation("grid", {
 	}
 });
 slate.bind("g:alt", grid);
+slate.bind("t:alt", tileAll);
 
 var undo = slate.operation("undo");
 slate.bind("u:alt", undo);
@@ -179,8 +249,12 @@ slate.bind("left:ctrl,alt", moveLeft);
 slate.bind("right:ctrl,alt", moveRight);
 
 //Throw bindings
-slate.bind("left:alt,cmd", throwLeftFullScreen);
-slate.bind("right:alt,cmd", throwRightFullScreen);
+slate.bind("left:alt,cmd", function(win) {
+  throwFullScreen(-1, win);
+});
+slate.bind("right:alt,cmd", function(win) {
+  throwFullScreen(1, win);
+});
 
 //Fullscreen toggle
 slate.bind("f:alt,cmd", fullScreenToggle);
